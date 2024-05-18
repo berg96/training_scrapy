20275223a84690795b68f3a8e986d758801d043e
Artem Kulikov <chigar2010@yandex.ru>
2024-05-18 20:37:03 +0200

    init

---
+++ training_scrapy/spiders/example.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    # Имя паука должно быть уникальным в рамках одного проекта.
+    name = "quotes"
+    # Список стартовых ссылок, с которых паук начнёт парсить данные.
+    start_urls = ['http://quotes.toscrape.com/',]
+
+    # Метод, загружающий и обрабатывающий каждую из стартовых ссылок.
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            # Для каждой найденной цитаты создаём и возвращаем словарь:
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('a.tag::text').getall(),
+            }
+
+            # По CSS-селектору ищем ссылку на следующую страницу.
+            next_page = response.css('li.next a::attr(href)').get()
+            if next_page is not None:
+                # Если ссылка нашлась, загружаем страницу по ссылке
+                # и вызываем метод parse() ещё раз.
+                yield response.follow(next_page, callback=self.parse)
